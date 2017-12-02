9504ce0d11a1007c52cecac3d73f89593a1bd84c
Jacob Senecal <jacobsenecal@JS-MBP.local>
2017-12-02 11:30:45 -0700

    content rating updates

--- Content_Rating.py
+++ Content_Rating.py
@@ -1,10 +1,25 @@
 import numpy as np
 import collections
 from sklearn.neighbors.kde import KernelDensity
+from scipy.stats.stats import f_oneway
 import matplotlib.pyplot as plt
 
 
 def calculate(data):
+    # Calculate summary statistics for common ratings (G, PG, PG-13, R)
+    stat = collections.namedtuple('stat', ('mean', 'min', 'max', 'stdev', 'count'))
+
+    rating_budget_stat = {}  # Budget Statistics associated with each rating
+    for key in data.budget.keys():
+        rating_budget_stat[key] = stat(np.mean(data.budget[key]), np.min(data.budget[key]), np.max(data.budget[key]),
+                                       np.std(data.budget[key]), len(data.budget[key]))
+
+    rating_revenue_stat = {} # Revenue statistics associated with each rating
+    for key in data.revenue.keys():
+        rating_revenue_stat[key] = stat(np.mean(data.revenue[key]), np.min(data.revenue[key]),
+                                        np.max(data.revenue[key]),
+                                        np.std(data.revenue[key]), len(data.budget[key]))
+
     # Calculate revenue probability distribution for each rating
     rating_revenue_dist = {}
     for key in data.revenue.keys():
@@ -38,13 +53,3 @@ def calculate(data):
         plt.plot(x_grid, np.exp(log_pdf))
         plt.title(key)
         plt.show()
-
-    # Calculate summary statistics for common ratings (G, PG, PG-13, R)
-    stat = collections.namedtuple('stat', ('mean', 'min', 'max', 'stdev'))
-
-    rating_budget_stat = {}
-    for key in data.budget.keys():
-        rating_budget_stat[key] = stat(np.mean(data.budget[key]), np.min(data.budget[key]), np.max(data.budget[key]),
-                                      np.std(data.budget[key]))
-
-    
